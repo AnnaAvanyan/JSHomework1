98147c4b31632387ae03f2b2c45c46824dead10b
AnnaAvanyan <74509053+AnnaAvanyan@users.noreply.github.com>
2022-02-23 00:27:25 +0400

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,21 @@
+function CompareStrings(s1,s2){
+
+    for (let i = 0; i < s1.length; i++) {
+        var is_found = false;
+        for (let j = 0; j < s2.length; j++) {
+            if(s1[i] == s2[j]){
+                 is_found = true;
+                 break;
+            }
+
+          } 
+          if(!is_found){
+              return false;
+          }
+      } 
+      return true;
+}
+
+var s1 = "name";
+var s2 = "anme";
+console.log(CompareStrings(s1,s2));
